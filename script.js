624f33f6d853d29116b957583e441fbd986b7e18
Mutahir Mustafa <s.mutahirmustafa@gmail.com>
2026-08-26 12:35:16 +0500

    remove comment

--- script.js
+++ script.js
@@ -19,9 +19,20 @@ box.push(document.getElementById("8"));
 let X = new Map(), O = new Map();
 let turn = "X",count=0;
 let winningcomb=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+ function all_either_X_or_O(box)
+ {
+    for (let i=0;i<box.length;i++)
+    {
+        if (box[i].innerHTML!="X"||box[i].innerHTML!="O")
+        {
+           return false;
+        }
+    }
+    return true;
+ }
 for (let i = 0; i < 9; i++) {
     box[i].addEventListener("click", function () {
-        // console.log(box[i].innerHTML);
+        
         if (turn == "X") {
             X.set(Number(box[i].innerHTML));
            if (box[i].innerHTML!="O"){ box[i].innerHTML = "X";
